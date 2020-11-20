81824ffb87468b7e73ac90eb21911674a34c0f03
Jon Sweeney <jonsweeney26@gmil.com>
2020-11-19 16:49:11 -0800

    Initial Commit

---
+++ PaperCrypto/PaperCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperCryptoApp.swift
+//  PaperCrypto
+//
+//  Created by Jonathan Sweeney on 11/19/20.
+//
+
+import SwiftUI
+
+@main
+struct PaperCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
